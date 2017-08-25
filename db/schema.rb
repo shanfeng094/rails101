bf2392ed17107217c1609ff27b4e86cc8ac9bc4a
shanfeng094 <1842114650@qq.com>
2017-08-25 14:35:53 +0800

    show groups creator info2

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170825045436) do
+ActiveRecord::Schema.define(version: 20170825051247) do
 
   create_table "groups", force: :cascade do |t|
     t.string   "title"
     t.text     "descrption"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
